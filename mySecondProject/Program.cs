f9e157c95d5cd37d239d3a8f9a54dbd0c5cd9d81
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2023-09-25 10:27:17 +0200

    Initial commit

---
+++ mySecondProject/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+
+namespace mySecondProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region welkoming messages
+            //welkom's by saying hello world
+            Console.WriteLine("Hello, World!");
+
+
+
+
+
+
+            #endregion
+
+            #region variables
+            //so, we are going to make some variables
+
+            //fist one is a string veriable!
+            string userName = "mark"; //declaration
+
+
+
+
+            #endregion
+
+        } // hi
+
+        /*
+         * 
+         * 
+         * hi 
+         * hi
+         */
+    }
+}
