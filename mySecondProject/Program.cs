8d29a8af3be8858d59e997c8eccd6e971141e1f7
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2023-09-25 10:52:34 +0200

    hi

--- mySecondProject/Program.cs
+++ mySecondProject/Program.cs
@@ -21,7 +21,21 @@ namespace mySecondProject
             //so, we are going to make some variables
 
             //fist one is a string veriable!
-            string userName = "mark"; //declaration
+            string userName = "mark";
+
+            //second one is character veriable
+            char myFarvoriteOneEver = ';';
+
+            //interger
+            int age = 41;
+
+            //float
+            float pi = 3.14159265358979323846264338327950288f;
+
+            //double
+            double simplepi = 3.1415;
+
+
 
 
 
