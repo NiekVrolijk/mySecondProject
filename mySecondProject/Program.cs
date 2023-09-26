c14fa252b57424998be58806f13cc98ccb845486
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2023-09-26 12:34:16 +0200

    hi

--- mySecondProject/Program.cs
+++ mySecondProject/Program.cs
@@ -21,13 +21,13 @@ namespace mySecondProject
             //so, we are going to make some variables
 
             //fist one is a string veriable!
-            string userName = "mark";
+            string userName = "Niek";
 
             //second one is character veriable
             char myFarvoriteOneEver = ';';
 
             //interge
-            int age = 41;
+            int age = 17;
 
             //float
             float pi = 3.14159265358979323846264338327950288f;
@@ -35,14 +35,67 @@ namespace mySecondProject
             //double
             double simplepi = 3.1415;
 
-            //boolian
+            //boolean
             bool lovesCat = true;
 
+            //user imput
+            string userImput;
+            string userAge;
+
+            //user imput variables
+            string userImputName;
+            string userImputPassword;
+
+            //v for expected values
+
+            string expectedName = "Niek";
+            string expectedPassword = "Password";
+
+            /*
+                        Console.WriteLine("something");
+                        Console.WriteLine(); //white space
+                        Console.WriteLine("something else");
+                        Console.WriteLine(userName);
+                        Console.WriteLine(myFarvoriteOneEver);
+                        Console.WriteLine(age);
+                        Console.WriteLine(pi);
+                        Console.WriteLine(simplepi);
+                        Console.WriteLine(lovesCat);
+                        Console.WriteLine();
+
+                        Console.WriteLine("hi my name is " + userName);
+                        Console.WriteLine("hi my name is {0}", userName);
+                        Console.WriteLine("hi my age is " + age);
+                        Console.WriteLine("hi my name is {0} and I am {1} years old", userName, age);
+                        Console.WriteLine($"my name is {userName} and i am {age} years old");
+            */
+
+            //age checker in progress
+
+            //Console.Write("please enter your name: ");
+            //userImput = Console.ReadLine();
+            //Console.WriteLine($"hi {userImput}");
+            //Console.Write("please also give me your age: ");
+            //userAge = Console.ReadLine();
+            //Console.WriteLine($"hi {userImput}; age {userAge}");
+           //yo
 
 
+            //password defender
+            Console.Write("please enter your name: ");
+            userImputName = Console.ReadLine();
 
+            Console.Write("please enter your password: ");
+            userImputPassword = Console.ReadLine();
 
-
+            if (userImputName == expectedName && userImputPassword == expectedPassword)
+            {
+                Console.WriteLine("wow it's you");
+            }
+            else
+            {
+                Console.WriteLine("how dare you, you fake");
+            }
             #endregion
 
         } // hi
