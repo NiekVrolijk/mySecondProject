65f010a45710e47792dc99a9d274fa37321c32a1
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2023-10-09 10:53:30 +0200

    that is not one of the given options

--- week3OrSmth/Program.cs
+++ week3OrSmth/Program.cs
@@ -22,25 +22,29 @@
             Console.WriteLine();
 
             choice = Console.ReadLine();
-            if (choice == "A" || choice == "a")
+            if (choice == "A" || choice == "a" || choice == "add")
             {
                 Calculator();
                 AddNumbers();
             }
-            else if (choice == "B" || choice == "b")
+            else if (choice == "B" || choice == "b" || choice == "subtract")
             {
                 Calculator();
                 SubtractNumbers();
             }
-            else if (choice == "C" || choice == "c")
+            else if (choice == "C" || choice == "c" || choice == "multiply")
             {
                 Calculator();
                 MultiplyNumbers();
             }
-            else if (choice == "D" || choice == "d")
+            else if (choice == "D" || choice == "d" || choice == "divide")
             {
                 Calculator();
                 DivideNumbers();
+            } else
+            {
+                Console.WriteLine();
+                Console.WriteLine("that is not one of the given options");
             }
 
             //methods
