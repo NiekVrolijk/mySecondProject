3f2d86d67d71b4a3ef1ac6194e8f5ee46a80e073
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2023-10-09 12:47:12 +0200

    thingy2

--- anotherOne/Program.cs
+++ anotherOne/Program.cs
@@ -1,43 +1,43 @@
 ﻿namespace anotherOne
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            string input;
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            string input;
 
-            //menu
-            Console.WriteLine("please choose one of the following options: ");
-            Console.WriteLine("A: Chocolate");
-            Console.WriteLine("B: Hotdog");
-            Console.WriteLine("C: Frog");
-            Console.WriteLine("D: Hippo");
-            Console.WriteLine();
+//            //menu
+//            Console.WriteLine("please choose one of the following options: ");
+//            Console.WriteLine("A: Chocolate");
+//            Console.WriteLine("B: Hotdog");
+//            Console.WriteLine("C: Frog");
+//            Console.WriteLine("D: Hippo");
+//            Console.WriteLine();
 
-            input = Console.ReadLine();
-            input = input.ToLower();
+//            input = Console.ReadLine();
+//            input = input.ToLower();
 
-            if (input == "a" || input == "chocolate") // .toLower() in c#
-            {
-                Console.WriteLine("you have chosen a chocolate flavored snack");
-            }
-            else if (input == "b" || input == "hotdog")
-            {
-                Console.WriteLine("you have chosen a hotdog flavored snack");
-            }
-            else if (input == "c" || input == "frog")
-            {
-                Console.WriteLine("you have chosen a frog flavored snack");
-            }
-            else if (input == "d" || input == "hippo")
-            {
-                Console.WriteLine("you have chosen a hippo flavored snack");
-            }
-            else
-            {
-                Console.WriteLine();
-                Console.WriteLine("INVALID OPTION!");
-            }
-        }
-    }
-}
+//            if (input == "a" || input == "chocolate")
+//            {
+//                Console.WriteLine("you have chosen a chocolate flavored snack");
+//            }
+//            else if (input == "b" || input == "hotdog")
+//            {
+//                Console.WriteLine("you have chosen a hotdog flavored snack");
+//            }
+//            else if (input == "c" || input == "frog")
+//            {
+//                Console.WriteLine("you have chosen a frog flavored snack");
+//            }
+//            else if (input == "d" || input == "hippo")
+//            {
+//                Console.WriteLine("you have chosen a hippo flavored snack");
+//            }
+//            else
+//            {
+//                Console.WriteLine();
+//                Console.WriteLine("INVALID OPTION!");
+//            }
+//        }
+//    }
+//}
---
+++ anotherTwo/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace anotherTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 10;
+            int anotherNum = 0;
+
+            AddsFive();
+            AddsFive();
+            AddsFive();
+            AddsFive();
+            AddsFive();
+
+
+            void AddsFive()
+            {
+                num += 5;
+                Console.WriteLine(num);
+            }
+
+            void AddsOneMoreEveryTime()
+            {
+                anotherNum += 1;
+                Console.WriteLine(num);
+            }
+
+        }
+    }
+}
