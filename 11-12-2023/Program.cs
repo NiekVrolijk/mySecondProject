cede0ac64e8021cef203e567e494c11e4743924a
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2023-12-11 13:00:35 +0100

    ahhh

--- 11-12-2023/Program.cs
+++ 11-12-2023/Program.cs
@@ -1,10 +1,11 @@
-﻿namespace _11_12_2023
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace _11_12_2023
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
             #region Q1
             ////num array
             //int[] numberArray;
@@ -39,6 +40,13 @@
             string[] playerNames = new string[4];
             int playerNum = 0;
 
+            int[] diceRoll = new int[4];
+
+            int highestNum = 0;
+            string highestPlayer = "test";
+
+            //var playerNumber = new (int, string)[4];
+
             //foreach (var item in playerNames) 
             //{
             //    playerNum++;
@@ -61,20 +69,42 @@
             {
                 Console.WriteLine("player " + i + " enter your name: ");
                 playerNames[i - 1] = Console.ReadLine();
-                Console.Write("player name saved");
                 Console.WriteLine();
-                Console.WriteLine();
-                if (i == 4)
-                {
-                    Console.WriteLine("the player names are:");
-                    Console.WriteLine("player 1: " + playerNames[0]);
-                    Console.WriteLine("player 2: " + playerNames[1]);
-                    Console.WriteLine("player 3: " + playerNames[2]);
-                    Console.WriteLine("player 4: " + playerNames[3]);
-                }
+                //if (i == 4)
+                //{
+                //    Console.WriteLine("the player names are:");
+                //    Console.WriteLine("player 1: " + playerNames[0]);
+                //    Console.WriteLine("player 2: " + playerNames[1]);
+                //    Console.WriteLine("player 3: " + playerNames[2]);
+                //    Console.WriteLine("player 4: " + playerNames[3]);
+                //}
             }
 
             #endregion
+
+            #region Q3
+            Console.WriteLine();
+            foreach (var item in playerNames) 
+            {
+                playerNum++;
+                Random random = new Random();
+                diceRoll[playerNum - 1] = random.Next(1, 100);
+                Console.WriteLine(playerNames[playerNum - 1] + " rolled: " + diceRoll[playerNum - 1]);
+
+                if (highestNum > diceRoll[playerNum - 1])
+                {
+                    highestNum = diceRoll[playerNum - 1];
+                    highestPlayer = playerNames[playerNum - 1];
+                }
+                //playerNumber[playerNum - 1].Item2 = playerNames[playerNum - 1];
+                //playerNumber[playerNum - 1].Item1 = diceRoll[playerNum - 1];
+            }
+            #endregion
+
+            
+            //highestNum = playerNumber.Max().Item1;
+            Console.WriteLine("highest number is: " + highestNum);
+            Console.WriteLine("winner is: " + highestPlayer);
 
         }
     }
