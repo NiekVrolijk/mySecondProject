e70c3fc9d81e3de67f481aa2e5bf957d47f4c53e
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2024-01-17 10:30:19 +0100

    leave

---
+++ 15-1-2024-praticeTest/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _15_1_2024_praticeTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region nvm
+            //int doubleUp = 10;
+            //int DoubleAmount = 3;
+
+            //Console.WriteLine($"this is the current number: {doubleUp}");
+
+            //for (int i = 0; i < DoubleAmount; i++)
+            //{
+            //    Double();
+            //}
+            //void Double()
+            //{
+            //    doubleUp *= 2;
+            //    Console.WriteLine($"this is the current number: {doubleUp}");
+            //}
+            #endregion
+
+
+        }
+    }
+}
