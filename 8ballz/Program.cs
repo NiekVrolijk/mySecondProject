430597bab82f1a30c5a84429fd7556f5b8994b91
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2024-02-05 12:30:44 +0100

    8 ballz

--- 8ballz/Program.cs
+++ 8ballz/Program.cs
@@ -1,10 +1,65 @@
-﻿namespace _8ballz
+﻿
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace _8ballz
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string anotherQuestion = "lol no";
+            List<string> respondes8Ball = new List<string>() {
+            "It is certain",
+            "It is decidedly so",
+            "Without a doubt",
+            "Yes definitely",
+            "You may rely on it",
+            "As I see it, yes",
+            "Most likely",
+            "Outlook good",
+            "Yes",
+            "Signs point to yes",
+            "Reply hazy, try again",
+            "Ask again later",
+            "Better not tell you now",
+            "Cannot predict now",
+            "Concentrate and ask again",
+            "Don’t count on it",
+            "My reply is no",
+            "My sources say no",
+            "Outlook not so good",
+            "Very doubtful" };
+
+            Console.WriteLine("what yes or no question would you like to have answered today: ");
+
+            Responds();
+
+            void Responds()
+            {
+                Console.ReadLine();
+                Random rndResponds = new Random();
+                int responds = rndResponds.Next(0, 20);
+                Console.WriteLine();
+                Console.WriteLine(respondes8Ball[responds]);
+                Console.WriteLine();
+
+
+                Console.WriteLine("would you like to ask another question?");
+                anotherQuestion = Console.ReadLine();
+                if (anotherQuestion == "yes" || anotherQuestion == "YES")
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("ok you may now ask another question: ");
+                    Responds();
+                } else
+                {
+                    Console.WriteLine("very well");
+                }
+            }
+
+
+
         }
     }
 }
---
+++ lists/Program.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+
+namespace lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<string> list = new List<string> { "markie", "aahhhh2", "the end of days" };
+            //foreach (string s in list)
+            //{
+            //    Console.WriteLine(s);
+            //}
+
+            //List<string> list2 = new List<string>();
+
+            ////list2.Add("ballz");
+
+            ////foreach (string s in list2)
+            ////{
+            ////    Console.WriteLine(s);
+            //////}
+
+            //Console.WriteLine("add sm ballz; how many ballz would you like to add");
+            //int numOfBallz = int.Parse(Console.ReadLine());
+            //for (int i = 0; i < numOfBallz; i++)
+            //{
+            //    Console.WriteLine("now you may add a ball: ");
+            //    list2.Add(Console.ReadLine());
+            //}
+
+            //Console.WriteLine();
+
+            //Console.WriteLine("these are your choosen ballz");
+            //foreach (string s in list2)
+            //{
+            //    Console.WriteLine(s);
+            //}
+
+            //Console.WriteLine();
+
+            //Console.WriteLine("now choose a ball you would like to 'take out'");
+            //list2.Remove(Console.ReadLine());
+
+            //Console.WriteLine("you now have these left: ");
+            //foreach (string s in list2)
+            //{
+            //    Console.WriteLine(s);
+            //}
+
+            //List<string> groceries = new List<string>();
+
+            //Console.WriteLine("how many groceries would you like to add: ");
+
+            //int numOfGroceries = int.Parse(Console.ReadLine());
+            //for (int i = 0; i < numOfGroceries; i++)
+            //{
+            //    Console.WriteLine("you may now add a new grocerie to the list: ");
+            //    groceries.Add(Console.ReadLine());
+            //}
+            //Console.WriteLine();
+
+            //Console.WriteLine("these are your choosen groceries:");
+            //foreach (string s in groceries)
+            //{
+            //    Console.WriteLine(s);
+            //}
+
+            //Console.WriteLine();
+            //if (groceries.Contains("bananas"))
+            //{
+            //    Console.WriteLine("BANANANAAAASSSSS!!!");
+            //} else
+            //{
+            //    Console.WriteLine("i have no bananas today");
+            //}
+
+            //Console.WriteLine();
+            //Console.WriteLine("i have removed: " + groceries[0]);
+            //groceries.Remove(groceries[0]);
+
+
+
+            //List<string> list1 = new List<string>() { "markie", "aahhhh2", "the end of days" };
+            //List<string> list2 = new List<string>() { "markie2", "aahhhh3", "the end of days2" };
+            //list1.AddRange(list2);
+
+            //foreach (string s in list1)
+            //{
+            //    Console.WriteLine(s);
+            //}
+
+            //list1.RemoveRange(0, 2);
+
+            //foreach (string s in list1)
+            //{
+            //    Console.WriteLine(s);
+            //}
+        }
+    }
+}
