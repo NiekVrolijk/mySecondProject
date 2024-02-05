66fe84ec731f29ccb268d74a9a99db53159136b0
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2024-02-05 09:46:45 +0100

    8 balls

--- scriptingtest2/Program.cs
+++ scriptingtest2/Program.cs
@@ -6,9 +6,25 @@
         {
 
             #region question1
+            int[] nums = new int[100];
+            int random;
+            int good = 0;
+            int bad = 0;
 
-
-
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Random rnd = new Random();
+                random = rnd.Next(1, 21);
+                nums[i] = random;
+                if (nums[i] > 10)
+                {
+                    good += 1;
+                } else
+                {
+                    bad += 1;
+                }
+            }
+            Console.WriteLine($"good: {good} + bad: {bad}");
 
             #endregion
         }
