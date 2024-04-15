ec3635057264b60ace5fed04ff939bda335c6a0e
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2024-04-15 13:35:44 +0200

    classes

---
+++ classesRock`5-4-2024/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace classesRock_5_4_2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            rock JustARock = new rock();
+
+            Console.WriteLine("the rock is called: " + JustARock.Name);
+            Console.WriteLine("the rock tastes: " + JustARock.Taste);
+        }
+    }
+}
---
+++ classesRock`5-4-2024/rock.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classesRock_5_4_2024
+{
+    internal class rock
+    {
+        private string name = "Rock"; //setter
+        private string taste = "satly";
+
+        public string Name //getter
+        {
+            get { return name; }
+        }
+
+        public string Taste
+        {
+            get { return taste; }
+        }
+
+        public rock()
+        {
+            name = "Rock";
+        }
+
+        public rock(string name)
+        {
+            this.name = name;
+        }
+    }
+}
